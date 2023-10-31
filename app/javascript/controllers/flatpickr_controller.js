1710399dfb13633b703bb016381ec7b4db07be71
Thibault Chassine <thibault.chassine@gmail.com>
2023-10-31 20:26:23 +0100

    flatpickr added to create-trip

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"
+import rangePlugin from "flatpickrRangePlugin"
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  static targets = [ "startTime", "endTime" ]
+
+  connect() {
+    flatpickr(this.startTimeTarget, {
+      enableTime: false,
+      plugins: [new rangePlugin({ input: "#end_time"})],
+      inline: true
+    })
+    flatpickr(this.endTimeTarget, {})
+  }
+}
